bea1cf3ab454514c36af520db012ba584ba748ff
annga <annga@DESKTOP-8BIL9IK>
2023-08-31 20:52:33 +1200

    Made changes to optimise forever home, added interaction, play, and trick

---
+++ src/foreverhome/Play.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package foreverhome;
+
+/**
+ *
+ * @author annga
+ */
+public class Play extends Interaction 
+{   
+    public Play(String playName, int levelUnlocked, String interactionDesc)
+    {
+        super(playName, levelUnlocked, "Play", interactionDesc);
+    }
+}
---
+++ src/foreverhome/Trick.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package foreverhome;
+
+/**
+ *
+ * @author annga
+ */
+public class Trick extends Interaction 
+{
+    public Trick(String trickName, int levelUnlocked, String interactionDesc)
+    {
+        super(trickName, levelUnlocked, "Trick", interactionDesc);
+    }
+}
